03327f69234c1521d1e8522de03df95545637462
Ulisses Moraes <ulissesmoraes74@gmail.com>
2023-11-10 17:59:29 -0400

    try | catch | finally

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿try
+{
+    string[] linhas = File.ReadAllLines("Arquivos/ArquivoLeitura.txt");
+
+    foreach (string linha in linhas)
+    {
+        Console.WriteLine(linha);
+    }
+}
+catch (FileNotFoundException ex)
+{
+    Console.WriteLine($"ERRO: Arquivo não localizado no diretório. {ex.Message}");
+}
+catch (DirectoryNotFoundException ex)
+{
+    Console.WriteLine($"ERRO: Diretório não localizado. {ex.Message}");
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Ocorreu uma exceção genérica. {ex.Message}");
+}
+finally
+{
+    Console.WriteLine("Fechar Conexão...");
+}
